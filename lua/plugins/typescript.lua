f3ae24ef001a3c8d3c73e0c70fd34ce2077ed10d
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-28 07:46:31 +0700

    add coq.nvim

---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,9 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+}
